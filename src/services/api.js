36cd58b8eec80e1bd85da767e705bf4ce2107d5d
AyalaKelerman <149535284+AyalaKelerman@users.noreply.github.com>
2025-09-01 13:05:03 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://node-school-management-system-khou.onrender.com',
+  baseURL: 'https://localhost:5000/',
 });
 
 API.interceptors.request.use(config => {
